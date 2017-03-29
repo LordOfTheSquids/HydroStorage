5c4107c8275c909b7e0a18c260d4f380d87b4934
mpollar <mpollar@purdue.edu>
2017-03-28 22:12:14 -0400

    Update turbine_loss.m

--- turbine_loss.m
+++ turbine_loss.m
@@ -1,3 +1,8 @@
 function eLost = turbine_loss(nTurb, eOut)
-    eLost = ((1 / nTurb) - 1) * eOut;
-end;
+    arraySize = size(nTurb);
+    arraySize = arraySize(2);
+    for k= 1:1:arraySize
+     eLost(1,k) = ((1 / nTurb(1,k)) - 1) * eOut;
+     %fprintf('eLost = %f', eLost(1,k));
+    end
+end
